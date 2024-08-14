770094f30daa5ce1ceed84f76ce8a48a453fe190
realcoin584 <realcoin584@gmail.com>
2024-08-14 09:59:22 -0700

    Update Transaction.js

--- wallet/Javascript/Transaction.js
+++ wallet/Javascript/Transaction.js
@@ -34,7 +34,7 @@ onValue(ref(db,"TotalNumberOfUser"),(snapshot)=>{
 
 
 setInterval(()=>{
-    if(TotalNumberOfUser.length != 0 && localStorage.getItem('Mycode')===null){
+    if(TotalNumberOfUser.length != 0 && localStorage.getItem('AddressId')===null){
         update(ref(getDatabase(),"TotalNumberOfUser"),{
             Total:TotalNumberOfUser[0].Total+1
         }).then(()=>{
@@ -60,7 +60,7 @@ setInterval(()=>{
                 From:''
             })
 
-            
+            localStorage.setItem('AddressId','')
         })
     }
 
